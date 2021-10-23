11f2695e7f39b168ff79e802df9d6d3cdbf24b4e
Michal <michal001d9@gmail.com>
2021-10-23 10:23:26 +0200

    randomizer x

--- started.go
+++ started.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
 	var name string
+	fmt.Println("Podaj swoje imie: ")
 	fmt.Scanln(&name)
 
-	x := 36
+	x := rand.Intn(100)
+	fmt.Println(x)
 	for i := 0; i < x; i++ {
 		if (i%5 == 0) && (i%7 == 0) {
 			fmt.Println("fizzbuzz")
